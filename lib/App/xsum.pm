67a7185173d3a783ca7263869034e60b8ff867f6
perlancar (@pc-home) <perlancar@gmail.com>
2016-11-23 06:17:30 +0700

    Finish first implementation

--- lib/App/xsum.pm
+++ lib/App/xsum.pm
@@ -11,6 +11,29 @@ our %SPEC;
 
 $SPEC{xsum} = {
     v => 1.1,
+    args => {
+        tag => {
+            summary => 'Create a BSD-style checksum',
+            schema => ['bool', is=>1],
+        },
+        check => {
+            summary => 'Read checksum from files and check them',
+            schema => ['bool', is=>1],
+        },
+        files => {
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'file',
+            schema => ['array*', of=>'filename*'],
+            req => 1,
+            pos => 0,
+            greedy => 1,
+        },
+        algorithm => {
+            schema => ['str*', in=>[qw/crc32 md5 sha1 sha224 sha256 sha384 sha512 sha512224 sha512256/]],
+            default => 'md5',
+            cmdline_aliases => {a=>{}},
+        },
+    },
     links => [
         {
             url => 'prog:shasum',
@@ -29,9 +52,69 @@ $SPEC{xsum} = {
             summary => 'Unix utility',
         },
     ],
+    'cmdline.skip_format' => 1,
 };
 sub xsum {
+    require File::Digest;
+    require Parse::Sums;
+
     my %args = @_;
+
+    my $num_success;
+    my $envres;
+    for my $file (@{ $args{files} }) {
+        if ($args{check}) {
+            my $res = Parse::Sums::parse_sums(filename => $file);
+            unless ($res->[0] == 200) {
+                $envres //= [
+                    500, "Some checksums files cannot be parsed"];
+                warn "Can't parse checksums from $file: $res->[1]\n";
+                next;
+            }
+            unless (@{ $res->[2] }) {
+                $envres //= [
+                    500, "Some checksums files don't contain any checksums"];
+                warn "No checksums found in $file".($res->[3]{'func.warning'} ? ": ".$res->[3]{'func.warning'} : "")."\n";
+                next;
+            }
+            warn "$file: ".$res->[3]{'func.warning'}."\n" if $res->[3]{'func.warning'};
+          ENTRY:
+            for my $entry (@{ $res->[2] }) {
+                my $digest_res = File::Digest::digest_file(
+                    file => $entry->{file}, algorithm => $entry->{algorithm});
+                unless ($digest_res) {
+                    $envres //= [
+                        500, "Some files' checksums cannot be checked"];
+                    warn "$entry->{file}: Cannot compute digest: $digest_res->[1]\n";
+                    next ENTRY;
+                }
+                if ($digest_res->[2] eq $entry->{digest}) {
+                    print "$entry->{file}: OK\n";
+                    $num_success++;
+                } else {
+                    $envres //= [
+                        500, "Some files did NOT match computed checksums"];
+                    print "$entry->{file}: FAILED\n";
+                }
+            }
+        } else {
+            my $res = File::Digest::digest_file(
+                file => $file, algorithm => $args{algorithm});
+            unless ($res->[0] == 200) {
+                warn "Can't checksum $file: $res->[1]\n";
+                next;
+            }
+            $num_success++;
+            if ($args{tag}) {
+                printf "%s (%s) = %s\n", uc($args{algorithm}), $file, $res->[2];
+            } else {
+                printf "%s  %s\n", $res->[2], $file;
+            }
+        }
+    }
+
+    return $envres if $envres;
+    $num_success ? [200] : [500, "All files failed"];
 }
 
 1;
@@ -42,7 +125,7 @@ sub xsum {
 See L<xsum>.
 
 
-=head1 SEE ALSO
+=head1 append:SEE ALSO
 
 Backend module: L<File::Digest>, which in turn uses L<Digest::CRC>,
 L<Digest::MD5>, and L<Digest::SHA>.
